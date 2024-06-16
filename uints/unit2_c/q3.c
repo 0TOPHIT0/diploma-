bc380848d6b3d77e2d5c7e8db52acf01d75d72f2
oTOPHITo <es-MohamedFathi2025@alexu.edu.eg>
2024-06-16 16:12:34 +0300

    assignment_1

---
+++ uints/unit2_c/q3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+int x,z;
+printf("enter two numbers:");
+scanf("%d %d",&x,&z);
+
+printf("the sum is :%d",x+z); 
+
+
+}
---
+++ uints/unit2_c/q4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+
+    float x,y,z;
+    printf("enter two number:");
+    scanf ("%f %f",&x,&y);
+z= x*y;
+    printf("the product is :%f",z);
+}
